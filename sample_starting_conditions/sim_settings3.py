3c20c5a46e5da9f51633596bc19b8fa2650b967e
Space <68457668+Infinity101-jpg@users.noreply.github.com>
2025-12-28 15:21:56 +0200

    Create sim_settings3.py

---
+++ sample_starting_conditions/sim_settings3.py
@@ -0,0 +1,16 @@
+
+Game_slowdown_percent = 40    # 0 to 100
+
+# OBJECT 1 (Blue Planet)
+Obj_1_col = "LIME"
+Obj_1_radius = 3
+Obj_1_weight = .0000008           
+Obj_1_coords = [150.0, -200.0] # Use floats
+Velocity1 = [-1.2, 0]    ## regs, op 
+
+# OBJECT 2 (Red Star)
+Obj_2_col = "GOLD"
+Obj_2_radius = 70
+Obj_2_weight = 1000000          
+Obj_2_coords = [0.0, 100.0]     
+Velocity2 = [0.0, 0.0]  
