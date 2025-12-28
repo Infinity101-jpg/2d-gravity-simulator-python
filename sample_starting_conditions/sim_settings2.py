2bde08290d2a467d5a445e37aa7f3a8bc8799607
Space <68457668+Infinity101-jpg@users.noreply.github.com>
2025-12-28 14:18:26 +0200

    Add files via upload

---
+++ sample_starting_conditions/sim_settings2.py
@@ -0,0 +1,18 @@
+
+
+Game_slowdown_percent = 20    # 0 to 100
+
+# OBJECT 1 (Blue Planet)
+Obj_1_col = "BLUE"
+Obj_1_radius = 10
+Obj_1_weight = 50000           
+Obj_1_coords = [-150.0, 00.0] # Use floats
+Velocity1 = [0, .5]    ## regs, op 
+
+# OBJECT 2 (Red Star)
+Obj_2_col = "RED"
+Obj_2_radius = 30
+Obj_2_weight = 200000         
+Obj_2_coords = [150.0, 0.0]     
+Velocity2 = [0.0, -.125]  
+
