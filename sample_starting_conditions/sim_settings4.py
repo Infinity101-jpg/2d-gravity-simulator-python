95579bbe055aa22923119be819bad2354c883eab
Space <68457668+Infinity101-jpg@users.noreply.github.com>
2025-12-28 15:54:24 +0200

    Create sim_settings4.py

---
+++ sample_starting_conditions/sim_settings4.py
@@ -0,0 +1,16 @@
+
+Game_slowdown_percent = 0    # 0 to 100
+
+# OBJECT 1 (Blue Planet)
+Obj_1_col = "SILVER"
+Obj_1_radius = 10
+Obj_1_weight = 10000          
+Obj_1_coords = [-270.0, 0.0] # Use floats
+Velocity1 = [0, 0]    ## regs, op 
+
+# OBJECT 2 (Red Star)
+Obj_2_col = "CYAN"
+Obj_2_radius = 10
+Obj_2_weight = 10000         
+Obj_2_coords = [270.0, 0.0]     
+Velocity2 = [0.0, 0.0]  
