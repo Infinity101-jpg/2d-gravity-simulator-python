76c3fada69ebe40565d0cb108e4cd72789c183b8
Space <68457668+Infinity101-jpg@users.noreply.github.com>
2025-12-28 14:59:33 +0200

    Update sim_settings1.py

--- sample_starting_conditions/sim_settings1.py
+++ sample_starting_conditions/sim_settings1.py
@@ -1,16 +1,16 @@
 
-Game_slowdown_percent = 20    # 0 to 100
+Game_slowdown_percent = 0    # 0 to 100
 
 # OBJECT 1 (Blue Planet)
 Obj_1_col = "PINK"
 Obj_1_radius = 10
-Obj_1_weight = .008           
+Obj_1_weight = .00000000008           
 Obj_1_coords = [-150.0, 100.0] # Use floats
 Velocity1 = [.5, .8]    ## regs, op 
 
 # OBJECT 2 (Red Star)
 Obj_2_col = "TEAL"
 Obj_2_radius = 30
-Obj_2_weight = 500000          
+Obj_2_weight = 1000000          
 Obj_2_coords = [150.0, -50.0]     
 Velocity2 = [0.0, 0.0]  
